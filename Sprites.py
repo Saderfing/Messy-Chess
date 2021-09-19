6ee00b2ed0da86b8fb24d87e0522f8fb30684b17
saderfing <saderfing@gmail.com>
2021-09-19 21:28:39 +0200

    pomme

--- Sprites.py
+++ Sprites.py
@@ -2,17 +2,30 @@ from random import randint
 
 class Map():
     def __init__(self):
-        self.map = 
-        [
-        [[],[],[],[],[]],
-        [[],[],[],[],[]],
-        [[],[],[],[],[]],
-        [[],[],[],[],[]],
+        self.map = [
+        [[],[],[],[]],
+        [[],[],[],[]],
+        [[],[],[],[]],
+        [[],[],[],[]]
                         ]
+        
+    def GetMap(self):
+        return self.map
+    
+    def __str__(self):
+        return f"""
+     {self.map[0][0][0]} | {self.map[0][0][1]} | {self.map[0][0][2]} | {self.map[0][0][3]}  
+    -------------------
+     {self.map[0][1][0]} | {self.map[0][1][1]} | {self.map[0][1][2]} | {self.map[0][1][3]} 
+    -------------------
+     {self.map[0][2][0]} | {self.map[0][2][1]} | {self.map[0][2][2]} | {self.map[0][2][3]} 
+    -------------------
+     {self.map[0][3][0]} | {self.map[0][3][1]} | {self.map[0][3][2]} | {self.map[0][3][3]} 
+    """
 
 
 class Piece():
-    def __init__(self, HP, streght, speed, range):
+    def __init__(self, HP, strenght, speed, range):
         
         self.HP = HP
         self.DF = None # ajout de la défence ?
@@ -31,9 +44,9 @@ class Piece():
 
 class Sponge(Piece):
     def __init__(self, HP, streght, speed, range):
-        super().__init__(, HP, streght, speed, range)
+        super().__init__(self, HP, streght, speed, range)
     
-    def Move(self)
+    def Move(self):
         super().Move()
     
     def Damage(self, strenght):
