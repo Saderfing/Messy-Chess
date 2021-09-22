c770b2ceb682752b1cd20a0e20129ff3346fd1fa
Saderfing <80517472+Saderfing@users.noreply.github.com>
2021-09-22 22:42:47 +0200

    Add files via upload

--- Sprites.py
+++ Sprites.py
@@ -2,48 +2,19 @@ from random import randint
 
 class Map():
     def __init__(self):
-        self.empty = "               "
-        self.map = {(0,0):self.empty, (0,1):self.empty, (0,2):self.empty, (0,3):self.empty, 
-                    (1,0):self.empty, (1,1):self.empty, (1,2):self.empty, (1,3):self.empty,
-                    (2,0):self.empty, (2,1):self.empty, (2,2):self.empty, (2,3):self.empty,
-                    (3,0):self.empty, (3,1):self.empty, (3,2):self.empty, (3,3):self.empty}
-        
-    def __str__(self):
-        return f"""
-                     |                 |                 |              
-     {self.map[0,0]} | {self.map[0,1]} | {self.map[0,2]} | {self.map[0,3]}  
-                     |                 |                 |
-    -------------------
-                     |                 |                 |
-     {self.map[1,3]} | {self.map[1,1]} | {self.map[1,2]} | {self.map[1,3]} 
-                     |                 |                 |
-    -------------------
-                     |                 |                 |
-     {self.map[2,0]} | {self.map[2,1]} | {self.map[2,2]} | {self.map[2,3]} 
-                     |                 |                 |
-    -------------------
-                     |                 |                 |
-     {self.map[3,0]} | {self.map[3,1]} | {self.map[3,2]} | {self.map[3,3]} 
-                     |                 |                 |
-    """
-    
-    
-    def GetMap(self):
-        return self.map
-
-    def SetObjectPosition(self, objName:str, pos=tuple):
-        if pos in self.map:
-            self.map[pos] = objName
-        else:
-            print("invalid position")
-
+        self.map = [
+        [[],[],[],[]],
+        [[],[],[],[]],
+        [[],[],[],[]],
+        [[],[],[],[]],
+                        ]
 
 
 class Piece():
-    def __init__(self, name:str, hp:int, strenght:int, speed:int, range:int):
-        self.name = name
-        self.hp = hp
-        self.df = None # ajout de la défence ?
+    def __init__(self, HP, strenght, speed, range):
+        
+        self.HP = HP
+        self.DF = None # ajout de la défence ?
         self.strenght = strenght
         self.speed = speed
         self.range = range
@@ -58,8 +29,8 @@ class Piece():
 
 
 class Sponge(Piece):
-    def __init__(self, name:str, hp:int, strenght:int, speed:int, range:int):
-        super().__init__(self, name, hp, strenght, speed, range)
+    def __init__(self, HP, streght, speed, range):
+        super().__init__( HP, streght, speed, range)
     
     def Move(self):
         super().Move()
--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+import pygame
 from pygame.mixer import fadeout
 import Sprites
 
-map = Sprites.Map()
-print(map)
-map.SetObjectPosition("eponge", (0,0))
-print(map)
+class Window():
+    def __init__(self):
+        self.width = 1280
+        self.height = 720
+        self.isRunning = True
+        self.FPS = 60
+        self.bg = None
+        self.screen = pygame.display.set_mode((self.width,self.height))
+    
+    def CreatGameWindow(self):
+        pygame.display.set_caption("Messy Chess")  
+        
+    
+    def GetGameStat(self):
+        return self.isRunning
+    
+    def SetGameStat(self, stat:bool):
+        self.isrunning = False
+    
+    def SetTick(self):
+        clock.tick(60)
+    
+    def GetBackground(self):
+        return self.bg
+    
+
+main = Window()
+main.CreatGameWindow()
+pygame.init()
+clock = pygame.time.Clock()
+
+while main.GetGameStat():
+    main.SetTick()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                main.SetGameStat(False)
+                pygame.quit()
