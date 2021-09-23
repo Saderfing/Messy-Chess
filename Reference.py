121e93889c95b3f40c968f8a6533037b7ed03606
Paspatate <82947263+Paspatate@users.noreply.github.com>
2021-09-23 18:54:27 +0200

    cadeau début pygame

--- Reference.py
+++ Reference.py
@@ -1,6 +1,21 @@
 import pygame
 
+class Ref():
+    def __init__(self, dimension:tuple):
+        self.dimension = dimension
+        self.SCREEN = pygame.display.set_mode(self.dimension) 
+    
+    def GetDim(self):
+        return self.dimension
+
+    def GetWidth(self):
+        return self.dimension[0]
+
+    def GetHeight(self):
+        return self.dimension[1]
+    
+    def GetScreen(self):
+        return self.SCREEN
+
 WIDTH = 1280
 HEIGHT = 720
-
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from pygame.mixer import fadeout
 import Sprites
+from Reference import Ref,WIDTH,HEIGHT
+import pygame
+
+ref = Ref((WIDTH,HEIGHT))
 
 map = Sprites.Map()
-print(map)
 map.SetObjectPosition("eponge", (0,0))
-print(map)
+
+class Window:
+    def __init__(self):
+        self.CHECKERBOARDIMG = pygame.image.load("assets/checkerboard.png").convert()
+        self.rectCheckerBoard = self.CHECKERBOARDIMG.get_rect()
+        self.rectCheckerBoard.x = self.rectCheckerBoard.width/2
+
+        self.bg = pygame.image.load("assets/bg.png").convert()
+    
+    def DrawAll(self):
+        pygame.Surface.blit(ref.GetScreen(),game.CHECKERBOARDIMG,(ref.GetWidth()/2 - game.rectCheckerBoard.x,0))
+
+
+
+
+run = True
+game = Window()
+while run:
+    
+
+
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+
+pygame.quit()
