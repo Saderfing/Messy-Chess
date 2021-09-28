d598c6aa3250c08f2745a63fa71d295f778c6f53
Paspatate <82947263+Paspatate@users.noreply.github.com>
2021-09-28 18:37:08 +0200

    ajout de patate

--- Sprites.py
+++ Sprites.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 class Map():
     def __init__(self):
         self.EMPTY = "               "
@@ -64,7 +65,7 @@ class Piece():
 
     def GetName(self):
         return self.name
-    
+
     def GetPos(self):
         return self.pos
 
@@ -74,22 +75,55 @@ class Piece():
             map.SetObjectPosition(self.name, newPos)
         else:
             print("already somthing")
-        
+
 
     def Damage(self, strenght):
         self.HP -= randint(1, 2)*strenght
+
+    def GetColorName(self):
+        return self.color+self.name
+
+class Patate(Piece):
+    def __init__(self, color, hp, strenght, speed, range, pos):
+        self.name = "Patate"
+        super().__init__(self.name,color,hp,strenght,speed,range,pos)
+
+    def Movement(self, newPos:tuple):
+        if newPos[0] == self.pos[0] and newPos[1] != self.pos[1]:
+            self.CheckAttack()
+            map.SetObjectPosition(self.GetColorName(),newPos,self.pos)
+        elif newPos[0] != self.pos[0] and newPos[1] == self.pos[1]:
+            self.CheckAttack()
+            map.SetObjectPosition(self.GetColorName(),newPos,self.pos)
+        else:
+            print("pas possible")
+
+    def CheckAttack(self):
+        localMap = map.GetMap()
+        if self.GetPos() in localMap.keys() and not map.IsEmpty(self.GetPos()):
+            print("attack", self.GetPos())
+
+        else:
+            print("nothing to attack")
+
+
 
 
 
 class Sponge(Piece):
     def __init__(self, name: str, color: str, hp: int, strenght: int, speed: int, range: int, pos:tuple):
         super().__init__(name, color, hp, strenght, speed, range, pos)
-        
-    
-    
+
+
+
 
 map = Map()
 eponge = Sponge("sponge", "White", 10, 10, 1, 1, (0,0))
 print(map)
-eponge.Move((1,0))
+patateBlanche = Patate("white",10,10,1,2,(0,0))
+patateNoire = Patate("black", 10, 10, 2, 2,(0,1))
+map.SetObjectPosition(patateBlanche.GetColorName(), patateBlanche.GetPos())
+map.SetObjectPosition(patateNoire.GetColorName(), patateNoire.GetPos())
+print(map)
+patateBlanche.Movement((0,1))
 print(map)
