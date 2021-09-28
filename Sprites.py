7fe1fd4ab4bcbd9276ae7571ccd372f5e6076bad
Paspatate <romigami@orange.fr>
2021-09-28 19:20:41 +0200

    map correctif et test si vide

--- Sprites.py
+++ Sprites.py
@@ -4,10 +4,10 @@ from random import randint
 class Map():
     def __init__(self):
         self.EMPTY = "               "
-        self.map = {(0,0):self.EMPTY, (0,1):self.EMPTY, (0,2):self.EMPTY, (0,3):self.EMPTY,
-                    (1,0):self.EMPTY, (1,1):self.EMPTY, (1,2):self.EMPTY, (1,3):self.EMPTY,
-                    (2,0):self.EMPTY, (2,1):self.EMPTY, (2,2):self.EMPTY, (2,3):self.EMPTY,
-                    (3,0):self.EMPTY, (3,1):self.EMPTY, (3,2):self.EMPTY, (3,3):self.EMPTY}
+        self.map = {(0,0):self.EMPTY, (1,0):self.EMPTY, (2,0):self.EMPTY, (3,0):self.EMPTY,
+                    (0,1):self.EMPTY, (1,1):self.EMPTY, (2,1):self.EMPTY, (3,1):self.EMPTY,
+                    (0,2):self.EMPTY, (1,2):self.EMPTY, (2,2):self.EMPTY, (3,2):self.EMPTY,
+                    (0,3):self.EMPTY, (1,3):self.EMPTY, (2,3):self.EMPTY, (3,3):self.EMPTY}
 
     def __str__(self):
         return f"""
@@ -89,11 +89,12 @@ class Patate(Piece):
         super().__init__(self.name,color,hp,strenght,speed,range,pos)
 
     def Movement(self, newPos:tuple):
-        if newPos[0] == self.pos[0] and newPos[1] != self.pos[1]:
-            self.CheckAttack()
+        localMap = map.GetMap()
+        if newPos[0] == self.pos[0] and newPos[1] != self.pos[1] and map.IsEmpty(newPos):
+            
             map.SetObjectPosition(self.GetColorName(),newPos,self.pos)
-        elif newPos[0] != self.pos[0] and newPos[1] == self.pos[1]:
-            self.CheckAttack()
+        elif newPos[0] != self.pos[0] and newPos[1] == self.pos[1] and map.IsEmpty(newPos):
+            
             map.SetObjectPosition(self.GetColorName(),newPos,self.pos)
         else:
             print("pas possible")
